6ef3cb46abdb6bf51c72ff7757319e2e590b3efa
Jo <jomadro25@gmail.com>
2025-03-19 16:57:47 +0000

    Added refresh token logic in generate token file

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (user) => {
     try {
 
         if (typeof user !== "object" || !user || !user._id) {
-            logger.error(`Error generating a JWT token!`);
+            logger.error(`Error generating an Access Token!`);
             throw new AppError("Token generation error", 400);
         }
 
@@ -16,17 +16,42 @@ const generateToken = (user) => {
 
             { id: user._id },
             process.env.JWT_SECRET_KEY,
-            { expiresIn: '1h' } // Token expires in 1 hour
+            { expiresIn: '15m' } // Token expires in 15 minutes
 
         );
+
     }
 
     catch (error) {
-        console.log("Token generation failed!");
-        logger.error(`Error generating a JWT token! Error: ${error}`);
+        logger.error(`Error generating an Access Token! Error: ${error}`);
         throw new AppError("Token generation error", 500);
     }
 
 };
 
-module.exports = { generateToken };
+const generateRefreshToken = (user) => {
+
+    try {
+
+        if (typeof user !== "object" || !user || !user._id) {
+            logger.error(`Error generating a Refresh Token!`);
+            throw new AppError("Token generation error", 400);
+        }
+
+        return jwt.sign(
+            { id: user._id },
+            process.env.JWT_REFRESH_SECRET,
+            { expiresIn: "7d" }); // Long lifespan
+
+    }
+
+    catch (error) {
+        logger.error(`Error generating a Refresh Token! Error: ${error}`);
+        throw new AppError("Token generation error", 500);
+    }
+
+};
+
+
+
+module.exports = { generateToken, generateRefreshToken };
